18e20b853ac52304bdcf070eb007daa65ad84c06
peppe-quint-boogaard <peppequintb@gmail.com>
2019-06-05 14:15:19 +0200

    404 page

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.get('/profile', (req, res) => res.render('profile'))
 app.get('/details/:id', (req, res) =>
   res.render('detail', { id: req.params.id })
 )
-
-app.get('*', function(req, res) {
-  res.status(404).render('pages/404')
-})
+//
+// app.get('*', function(req, res) {
+//   res.status(404).render('pages/404')
+// })
 
 app.listen(port, () => console.log(`Wikiminds listening on port ${port}!`))
