781fea6b687aa6941169a367a01d96d119921095
peppe-quint-boogaard <peppequintb@gmail.com>
2019-06-17 15:21:22 +0200

    issue with nav item active

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,20 @@
 // console log to see if the index.js has loaded correctly
 
-console.log('index.js')
+console.log("index.js");
+
+const navList = document.querySelector(".m-nav--list");
+
+navList.addEventListener("click", function(event) {
+  let navItem = document.querySelectorAll(".m-list--item");
+  for (let i = 0; i < navItem.length; i++) {
+    if (event.target.dataset.id === navItem[i].dataset.id) {
+      if (event.target.className === "m-list--item m-item--inactive") {
+        event.target.className = "m-list--item m-item--active";
+      } else {
+        event.target.className = "m-list--item m-item--inactive";
+      }
+    } else {
+      navItem[i].className = "m-list--item m-item--inactive";
+    }
+  }
+});
