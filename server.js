33b702e84a99595b3d4b4106c6cb27c25fd1317a
peppe-quint-boogaard <peppequintb@gmail.com>
2019-06-18 14:00:52 +0200

    error with quotes

--- server.js
+++ server.js
@@ -1,173 +1,173 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const authentication = require('./authentication')
-const data = require('./data')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authentication = require("./authentication");
+const data = require("./data");
+require("dotenv").config();
 
-var session = require('express-session')
+var session = require("express-session");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // initialize session
 app.use(
   session({
-    secret: 'wikiminds',
+    secret: "wikiminds",
     resave: true,
     saveUninitialized: false
   })
-)
+);
 
 // create a session object to check whether a user is logged in on all pages
 app.use(function(req, res, next) {
-  res.locals.session = req.session
-  next()
-})
+  res.locals.session = req.session;
+  next();
+});
 
 // body parser for form data
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
-)
+);
 // connect to MongoDB hosted by mlab
-mongoose.connect(process.env.DB_MONGOLINK, { useNewUrlParser: true })
+mongoose.connect(process.env.DB_MONGOLINK, { useNewUrlParser: true });
 
 // define static folder
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // view engine set up
-app.set('view engine', 'ejs')
-app.set('views', './public/views/pages')
+app.set("view engine", "ejs");
+app.set("views", "./public/views/pages");
 
 // home route
-app.get('/', (req, res) =>
-  data.handler.getIssues().then(issues => res.render('index', { data: issues }))
-)
+app.get("/", (req, res) =>
+  data.handler.getIssues().then(issues => res.render("index", { data: issues }))
+);
 
 // detail page route
-app.get('/details/:id', (req, res) =>
+app.get("/details/:id", (req, res) =>
   data.handler.getDetail(req.params.id).then(issue => {
     data.handler.getUser(issue.owner).then(user => {
-      res.render('detail', { issue: issue, owner: user })
-    })
+      res.render("detail", { issue: issue, owner: user });
+    });
   })
-)
+);
 
 // liking an issue
-app.get('/details/:id/like', (req, res) => {
+app.get("/details/:id/like", (req, res) => {
   if (req.session.userId) {
     data.handler.addLike(req.params.id, req.session.userId).then(issue => {
       if (issue) {
-        res.render('message', {
-          message: 'You liked ' + issue.title,
-          redirect: '/details/' + issue._id
-        })
+        res.render("message", {
+          message: "You liked " + issue.title,
+          redirect: "/details/" + issue._id
+        });
       } else {
-        res.render('message', {
-          message: 'You already liked this post',
-          redirect: '/'
-        })
+        res.render("message", {
+          message: "You already liked this post",
+          redirect: "/"
+        });
       }
-    })
+    });
   } else {
-    res.render('message', {
-      message: 'You need to be logged in to like an issue',
-      redirect: '/login'
-    })
+    res.render("message", {
+      message: "You need to be logged in to like an issue",
+      redirect: "/login"
+    });
   }
-})
+});
 
 // delete issue
-app.get('/delete/:id', (req, res) => {
+app.get("/delete/:id", (req, res) => {
   if (req.session.userId) {
     data.handler.deleteIssue(req.params.id, req.session.userId).then(issue => {
       if (issue) {
-        res.render('message', {
-          message: 'Issue with name ' + issue.title + ' has been deleted',
-          redirect: '/'
-        })
+        res.render("message", {
+          message: "Issue with name " + issue.title + " has been deleted",
+          redirect: "/"
+        });
       } else {
-        res.render('message', {
-          message: 'Permission denied',
-          redirect: '/'
-        })
+        res.render("message", {
+          message: "Permission denied",
+          redirect: "/"
+        });
       }
-    })
+    });
   } else {
-    res.render('message', {
-      message: 'You are not logged in',
-      redirect: '/'
-    })
+    res.render("message", {
+      message: "You are not logged in",
+      redirect: "/"
+    });
   }
-})
+});
 
 // profile routes
-app.get('/profile', (req, res) => {
+app.get("/profile", (req, res) => {
   if (req.session.userId) {
     data.handler.getUser(req.session.userId).then(user => {
       data.handler.getIssuesForUser(user._id).then(issues => {
-        res.render('profile', { user: user, issues: issues })
-      })
-    })
+        res.render("profile", { user: user, issues: issues });
+      });
+    });
   } else {
-    res.redirect('/login')
+    res.redirect("/login");
   }
-})
+});
 
 // detail page per user
-app.get('/users/:id', (req, res) => {
+app.get("/users/:id", (req, res) => {
   data.handler.getUser(req.params.id).then(user => {
     data.handler.getIssuesForUser(user._id).then(issues => {
-      res.render('user', { user: user, issues: issues })
-    })
-  })
-})
+      res.render("user", { user: user, issues: issues });
+    });
+  });
+});
 
 // register
-app.get('/register', (req, res) => res.render('register'))
+app.get("/register", (req, res) => res.render("register"));
 
 // login
-app.get('/login', (req, res) => res.render('login'))
+app.get("/login", (req, res) => res.render("login"));
 
 // on logout
-app.get('/logout', function(req, res, next) {
+app.get("/logout", function(req, res, next) {
   if (req.session) {
     // delete session object
     req.session.destroy(function(err) {
       if (err) {
-        return next(err)
+        return next(err);
       } else {
-        return res.render('message', {
-          message: 'You are now logged out',
-          redirect: '/'
-        })
+        return res.render("message", {
+          message: "You are now logged out",
+          redirect: "/"
+        });
       }
-    })
+    });
   }
-})
+});
 
 // route for new issues
-app.get('/issue', (req, res) => {
+app.get("/issue", (req, res) => {
   if (req.session.userId) {
-    res.render('issue')
+    res.render("issue");
   } else {
-    return res.render('message', {
-      message: 'You need to be logged in to create an issue',
-      redirect: '/'
-    })
+    return res.render("message", {
+      message: "You need to be logged in to create an issue",
+      redirect: "/"
+    });
   }
-})
+});
 // 404
-app.get('*', function(req, res) {
-  res.status(404).render('404')
-})
+app.get("*", function(req, res) {
+  res.status(404).render("404");
+});
 
 // post routes for form submissions
-app.post('/register', authentication.register)
-app.post('/login', authentication.login)
-app.post('/newissue', data.upload)
+app.post("/register", authentication.register);
+app.post("/login", authentication.login);
+app.post("/newissue", data.upload);
 
-app.listen(port, () => console.log(`Wikiminds listening on port ${port}!`))
+app.listen(port, () => console.log(`Wikiminds listening on port ${port}!`));
